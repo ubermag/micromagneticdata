97fd370c4e5f383698ed1e68e892d8a4d319978e
Marijan Beg <marijan.beg@xfel.eu>
2018-10-24 14:48:03 +0200

    Add Dockerfile for Binder and fix .test().

--- micromagneticdata/__init__.py
+++ micromagneticdata/__init__.py
@@ -4,7 +4,7 @@ from .micromagneticdata import MicromagneticData
 
 
 def test():
-    return pytest.main(["-v", "--pyargs", "micromagneticmodel"])  # pragma: no cover
+    return pytest.main(["-v", "--pyargs", "micromagneticdata"])  # pragma: no cover
 
 __version__ = pkg_resources.get_distribution(__name__).version
 __dependencies__ = pkg_resources.require(__name__)
