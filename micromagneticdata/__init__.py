2bbdc2844571fe3f0218ab867a7a004617fa10d1
Marijan Beg <marijan.beg@xfel.eu>
2018-10-24 14:43:41 +0200

    Extend __init__.py

--- micromagneticdata/__init__.py
+++ micromagneticdata/__init__.py
@@ -1,1 +1,10 @@
+import pytest
+import pkg_resources
 from .micromagneticdata import MicromagneticData
+
+
+def test():
+    return pytest.main(["-v", "--pyargs", "micromagneticmodel"])  # pragma: no cover
+
+__version__ = pkg_resources.get_distribution(__name__).version
+__dependencies__ = pkg_resources.require(__name__)
