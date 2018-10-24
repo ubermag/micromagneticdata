1becb63077a24a66363313bb019bf0cb311f60b5
Marijan Beg <marijan.beg@xfel.eu>
2018-10-24 14:52:20 +0200

    Remove oommfc from test.

--- micromagneticdata/tests/test_micromagneticdata.py
+++ micromagneticdata/tests/test_micromagneticdata.py
@@ -2,16 +2,14 @@ import os
 import glob
 import shutil
 import pytest
-import oommfc as oc
 import micromagneticmodel as mm
-import pytest
 import micromagneticdata as md
 
 @pytest.mark.oommf
 def test_micromagneticdata():
     name = "micromagneticdata"
 
-    system = oc.System(name=name)
+    system = mm.System(name=name)
 
     assert md.MicromagneticData(system).name == name
     assert md.MicromagneticData('micromagneticdata').name == name
