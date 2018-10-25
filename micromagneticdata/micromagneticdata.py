e9c1a882e7399d04bba7aaa822b4a54a6201981b
Marijan Beg <marijan.beg@xfel.eu>
2018-10-25 12:43:34 +0200

    Update.

--- micromagneticdata/micromagneticdata.py
+++ micromagneticdata/micromagneticdata.py
@@ -1,12 +1,19 @@
 import micromagneticmodel as mm
 
-class MicromagneticData:
-	
-	def __init__(self, data):
 
-		if isinstance(data, mm.System):
-			self.name = data.name
-		elif isinstance(data, str):
-			self.name = data
-		else:
-			raise TypeError("Accept only mm.System or string")
+class MicromagneticData:
+    """
+    Examples
+    --------
+    Simple import.
+
+    >>> import micromagneticdata
+
+    """
+    def __init__(self, data):
+        if isinstance(data, mm.System):
+            self.name = data.name
+        elif isinstance(data, str):
+            self.name = data
+        else:
+            raise TypeError("Accept only mm.System or string")
