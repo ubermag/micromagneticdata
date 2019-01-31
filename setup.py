8b74316bde4b41d32832888481741db07747a979
Marijan Beg <marijan.beg@xfel.eu>
2019-01-31 13:58:57 +0100

    Remove space.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     author='Marijan Beg, Sergii Mamedov, Thomas Kluyver, and Hans Fangohr',
     author_email='jupyteroommf@gmail.com',
     packages=setuptools.find_packages(),
-    include_package_data = True,
+    include_package_data=True,
     install_requires=['ipywidgets',
                       'oommfodt',
                       'discretisedfield'],
