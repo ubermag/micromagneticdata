d364484f77e4ce33cddb14098dc980414b256986
Marijan Beg <m.beg@soton.ac.uk>
2020-06-14 23:30:55 +0100

    Tests.

--- micromagneticdata/tests/test_drive.py
+++ micromagneticdata/tests/test_drive.py
@@ -1,41 +1,63 @@
 import os
 import types
-import pandas as pd
+import ipywidgets
 import discretisedfield as df
 import micromagneticdata as md
 
-os.chdir(os.path.dirname(__file__))
 
-name = 'test_sample'
-number = 0
-drive = md.Drive(name, number)
+class TestDrive:
+    def setup(self):
+        self.dirname = os.path.dirname(__file__)
+        self.name = 'test_sample'
 
+    def test_init(self):
+        drive = md.Drive(name=self.name, number=0, dirname=self.dirname)
 
-def test_m0():
-    assert isinstance(drive.m0, df.Field)
+        assert isinstance(drive, md.Drive)
 
+    def test_info(self):
+        drive = md.Drive(name=self.name, number=1, dirname=self.dirname)
 
-def test_mif():
-    assert isinstance(drive.mif, str)
+        assert isinstance(drive.info, dict)
+        assert drive.info['drive_number'] == 1
 
+    def test_mif(self):
+        drive = md.Drive(name=self.name, number=2, dirname=self.dirname)
 
-def test_info():
-    assert isinstance(drive.info, dict)
+        assert isinstance(drive.mif, str)
+        assert 'MIF' in drive.mif
 
+    def test_m0(self):
+        drive = md.Drive(name=self.name, number=3, dirname=self.dirname)
 
-def test_dt():
-    assert isinstance(drive.dt, pd.DataFrame)
+        assert isinstance(drive.m0, df.Field)
 
+    def test_n(self):
+        drive = md.Drive(name=self.name, number=4, dirname=self.dirname)
 
-def test_step_number():
-    assert isinstance(drive.step_number, int)
+        assert isinstance(drive.n, int)
+        assert drive.n == 5
 
+    def test_step_filenames(self):
+        drive = md.Drive(name=self.name, number=0, dirname=self.dirname)
 
-def test_step_filenames():
-    assert isinstance(drive.step_filenames, types.GeneratorType)
-    assert all(isinstance(fn, str) for fn in drive.step_filenames)
+        assert isinstance(drive.step_filenames, types.GeneratorType)
+        assert len(list(drive.step_filenames)) == 25
+        assert all([isinstance(i, str) for i in drive.step_filenames])
 
+    def test_step(self):
+        drive = md.Drive(name=self.name, number=0, dirname=self.dirname)
 
-def test_step_fields():
-    assert isinstance(drive.step_fields, types.GeneratorType)
-    assert all(isinstance(f, df.Field) for f in drive.step_fields)
+        for i in range(drive.n):
+            assert isinstance(drive.step(i), df.Field)
+
+    def test_iter(self):
+        drive = md.Drive(name=self.name, number=0, dirname=self.dirname)
+
+        assert len(list(drive)) == 25
+        assert all([isinstance(i, df.Field) for i in drive])
+
+    def test_slider(self):
+        drive = md.Drive(name=self.name, number=0, dirname=self.dirname)
+
+        assert isinstance(drive.slider(), ipywidgets.IntSlider)
