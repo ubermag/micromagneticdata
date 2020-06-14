9c193670ee2aea5e9578b315c375b1d3f4ef24e2
Marijan Beg <m.beg@soton.ac.uk>
2020-06-15 00:50:39 +0100

    Update version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setuptools.setup(
     name='micromagneticdata',
-    version='0.1',
+    version='0.2',
     description=('Python tools for analysing micromagnetic '
                  'data in a Jupyter notebook.'),
     long_description=long_description,
