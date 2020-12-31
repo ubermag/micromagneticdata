d11c50399591f2b96770091c0ee4a0d2b9b94861
marijanbeg <m.beg@soton.ac.uk>
2020-12-31 23:28:18 +0000

    PEP8.

--- micromagneticdata/__init__.py
+++ micromagneticdata/__init__.py
@@ -20,5 +20,6 @@ def test():
     return pytest.main(['-v', '--pyargs',
                         'micromagneticdata', '-l'])  # pragma: no cover
 
+
 __version__ = pkg_resources.get_distribution(__name__).version
 __dependencies__ = pkg_resources.require(__name__)
