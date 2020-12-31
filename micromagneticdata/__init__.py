dc0a083554e06782cf06be0007f9d01679b1dceb
marijanbeg <m.beg@soton.ac.uk>
2020-12-31 23:23:43 +0000

    Docs.

--- micromagneticdata/__init__.py
+++ micromagneticdata/__init__.py
@@ -1,12 +1,24 @@
+"""Main package"""
 import pytest
 import pkg_resources
 from .data import Data
 from .drive import Drive
 
-__version__ = pkg_resources.get_distribution(__name__).version
-__dependencies__ = pkg_resources.require(__name__)
-
 
 def test():
+    """Run all package tests.
+
+    Examples
+    --------
+    1. Run all tests.
+
+    >>> import micromagneticdata
+    ...
+    >>> # micromagneticdata.test()
+
+    """
     return pytest.main(['-v', '--pyargs',
-                        'micromagneticdata'])  # pragma: no cover
+                        'micromagneticdata', '-l'])  # pragma: no cover
+
+__version__ = pkg_resources.get_distribution(__name__).version
+__dependencies__ = pkg_resources.require(__name__)
