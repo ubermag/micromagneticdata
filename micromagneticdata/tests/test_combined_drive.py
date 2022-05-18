b2a13e67abdf757c68bd1ab0d2d12e4f52da5b9b
Martin Lang <m.lang@soton.ac.uk>
2022-05-18 17:37:53 +0200

    Add tests for CombinedDrive

---
+++ micromagneticdata/tests/test_combined_drive.py
@@ -0,0 +1,123 @@
+import itertools
+import os
+
+import discretisedfield as df
+import numpy as np
+import pytest
+import ubermagtable as ut
+import xarray as xr
+
+import micromagneticdata as md
+
+
+class TestDrive:
+    # drives 0, 1, 2, 4: TimeDriver
+    # drives 3, 5: MinDriver
+    # drives 6: HysteresisDriver
+    def setup(self):
+        self.dirname = os.path.join(os.path.dirname(__file__), "test_sample")
+        self.name = "system_name"
+        self.data = md.Data(name=self.name, dirname=self.dirname)
+        self.combined_drives = [
+            self.data[0] << self.data[1],
+            self.data[3] << self.data[5],
+            self.data[6] << self.data[6],
+        ]
+
+    def test_init(self):
+        combined_drive = md.CombinedDrive(self.data[0], self.data[1])
+        assert isinstance(combined_drive, md.CombinedDrive)
+
+    def test_repr(self):
+        for combined in self.combined_drives:
+            assert isinstance(repr(combined), str)
+            assert "CombinedDrive" in repr(combined)
+            assert "Drive" in repr(combined)
+
+    def test_x(self):
+        for combined in self.combined_drives:
+            assert isinstance(combined.x, str)
+
+            assert combined.x in ["t", "iteration", "B_hysteresis"]
+
+        self.combined_drives[0].x = "mx"
+        with pytest.raises(ValueError):
+            self.combined_drives[0].x = "wrong"
+
+    def test_info(self):
+        for combined in self.combined_drives:
+            assert isinstance(combined.info, dict)
+            assert isinstance(combined.info["drive_numbers"], list)
+            assert combined.info["driver"] in [
+                "MinDriver",
+                "TimeDriver",
+                "HysteresisDriver",
+            ]
+
+    def test_m0(self):
+        for drive in self.combined_drives:
+            assert isinstance(drive.m0, df.Field)
+
+    def test_table(self):
+        for drive in self.combined_drives:
+            assert isinstance(drive.table, ut.Table)
+            assert drive.table.x == drive.x
+
+    def test_n(self):
+        assert self.combined_drives[0].n == 50
+        assert self.combined_drives[1].n == 2
+        assert self.combined_drives[2].n == 42
+
+    def test_getitem(self):
+        for i in range(self.combined_drives[1].n):
+            assert isinstance(self.combined_drives[1][i], df.Field)
+
+    def test_iter(self):
+        for drive in self.combined_drives:
+            for m in drive:
+                assert isinstance(m, df.Field)
+
+        assert len(list(self.combined_drives[0])) == 50
+
+    def test_lshift(self):
+        # drives 0, 1, 2, 4: TimeDriver
+        # drives 3, 5: MinDriver
+        # drives 6: HysteresisDriver
+        for d1, d2 in itertools.chain(
+            zip(self.combined_drives, self.combined_drives),
+            zip(self.combined_drives, [self.data[0], self.data[3], self.data[6]]),
+        ):
+            combined = d1 << d2
+            assert isinstance(combined, md.CombinedDrive)
+            assert len(combined.drives) == 3
+            assert combined.info["driver"] == self.data[d1].info["driver"]
+            assert combined.x == self.data[d1].x
+            assert len(combined.table.data) == combined.n
+
+        # different independent variable
+        with pytest.raises(ValueError):
+            self.combined_drives[0] << self.data[3]
+        with pytest.raises(ValueError):
+            self.combined_drives[0] << self.combined_drives[1]
+
+    def test_to_xarray(self):
+        for drive in self.combined_drives:
+            assert isinstance(drive.to_xarray(), xr.DataArray)
+            assert all(
+                item in drive.to_xarray().attrs.items() for item in drive.info.items()
+            )
+            if len(drive._step_files) != 1:
+                assert len(drive.to_xarray()[drive.table.x]) == len(drive._step_files)
+                assert np.allclose(
+                    drive.to_xarray()[drive.table.x].values,
+                    drive.table.data[drive.table.x].to_numpy(),
+                )
+
+            if drive.info["driver"] == "HysteresisDriver":
+                assert all(
+                    np.allclose(
+                        drive.to_xarray()[f"B{i}_hysteresis"].values,
+                        drive.table.data[f"B{i}_hysteresis"].to_numpy(),
+                    )
+                    for i in "xyz"
+                )
