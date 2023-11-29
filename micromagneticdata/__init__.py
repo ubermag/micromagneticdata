9464ec43e16607a13b7b42c3b46ed0b8e4d21143
Martin Lang <martin.lang@mpsd.mpg.de>
2023-11-29 15:54:35 +0100

    Remove calculator-specific drives
    To make micromagneticdata more general we remove all calculator specific reading
    functionality and move it to the respective adapter packages. This avoids
    changing micromagneticdata when we add a new calculator and simplifies outside
    contributions/extensions of Ubermag

--- micromagneticdata/__init__.py
+++ micromagneticdata/__init__.py
@@ -8,8 +8,6 @@ from .abstract_drive import AbstractDrive as AbstractDrive
 from .combined_drive import CombinedDrive as CombinedDrive
 from .data import Data as Data
 from .drive import Drive as Drive
-from .mumax3drive import Mumax3Drive as Mumax3Drive
-from .oommfdrive import OOMMFDrive as OOMMFDrive
 
 __version__ = importlib.metadata.version(__package__)
 
