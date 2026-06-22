71ba52a7f27b46eae75f6a90dde4c2e99b3c7e07
Martin Lang <martin.lang@mpsd.mpg.de>
2026-06-22 09:31:49 +0200

    Update tests for Data

--- micromagneticdata/tests/test_data.py
+++ micromagneticdata/tests/test_data.py
@@ -1,78 +1,92 @@
-import os
+import importlib.metadata
 import textwrap
-from pathlib import Path
 
 import ipywidgets
 import pandas as pd
 import pytest
 
-import micromagneticdata as md
+import micromagneticdata as mdata
+from .mock_data import create_drive, mock_entry_points
 
 
-class TestData:
-    def setup_method(self):
-        self.dirname = os.path.join(os.path.dirname(__file__), "test_sample")
-        self.name = "rectangle"
-        self.data = md.Data(name=self.name, dirname=self.dirname)
-        self.N_SAMPLES = 7
+@pytest.fixture
+def data(tmp_path, monkeypatch):
+    monkeypatch.setattr(importlib.metadata, "entry_points", mock_entry_points)
 
-    def test_init(self):
-        assert isinstance(self.data, md.Data)
+    system_name = "test_system"
+    create_drive(tmp_path, system_name, 0, "iteration", n_steps=1)
+    create_drive(tmp_path, system_name, 1, "t", n_steps=20)
+    create_drive(tmp_path, system_name, 2, "t", n_steps=5)
+    create_drive(tmp_path, system_name, 3, "iteration", n_steps=10)
+    return mdata.Data(name=system_name, dirname=tmp_path)
 
-        with pytest.raises(IOError):
-            md.Data(path=Path(self.dirname) / "nonexistent")
 
-        with pytest.raises(IOError):
-            md.Data(name="wrong", dirname=self.dirname)
+def test_init(data):
+    assert isinstance(data, mdata.Data)
 
-    def test_repr(self):
-        assert isinstance(repr(self.data), str)
-        assert "Data" in repr(self.data)
+    with pytest.raises(IOError):
+        mdata.Data(path=data.dirname / "nonexistent")
 
-    def test_info(self):
-        assert isinstance(self.data.info, pd.DataFrame)
-        assert len(self.data.info.index) == self.N_SAMPLES
+    with pytest.raises(IOError):
+        mdata.Data(name="wrong", dirname=data.dirname)
 
-    def test_info_missing_corrupt(self, tmp_path):
-        for i in range(3):
-            (tmp_path / "system" / f"drive-{i}").mkdir(parents=True)
 
-        # missing info.json for drive-0
+def test_repr(data):
+    assert isinstance(repr(data), str)
+    assert "Data" in repr(data)
 
-        # broken info.json for drive-1
-        (tmp_path / "system" / "drive-1" / "info.json").write_text('{"drive_number": 1')
 
-        # correct info.json for drive-2
-        info_text = textwrap.dedent(
-            """
-            {
-                "drive_number": 2,
-                "date": "2025-05-31",
-                "time": "20:29:33",
-                "start_time": "2025-05-31T20:29:33",
-                "adapter": "oommfc",
-                "adapter_version": "0.65.0",
-                "driver": "MinDriver",
-                "end_time": "2025-05-31T20:29:33",
-                "elapsed_time": "00:00:01",
-                "success": true}
-            """
-        )
-        (tmp_path / "system" / "drive-2" / "info.json").write_text(info_text)
+def test_info(data):
+    assert isinstance(data.info, pd.DataFrame)
+    assert len(data.info.index) == 4
 
-        data = md.Data(name="system", dirname=str(tmp_path))
-        info = data.info
-        assert info["info.json"].to_list() == ["missing", "corrupt", "available"]
 
-    def test_n(self):
-        assert self.data.n == self.N_SAMPLES
+def test_info_missing_corrupt(monkeypatch, tmp_path):
+    monkeypatch.setattr(importlib.metadata, "entry_points", mock_entry_points)
 
-    def test_getitem(self):
-        for i in range(-self.N_SAMPLES, self.N_SAMPLES):
-            assert isinstance(self.data[i], md.Drive)
+    for i in range(3):
+        (tmp_path / "system" / f"drive-{i}").mkdir(parents=True)
 
-    def test_iter(self):
-        assert len(list(self.data)) == self.N_SAMPLES
+    # missing info.json for drive-0
 
-    def test_selector(self):
-        assert isinstance(self.data.selector(), ipywidgets.BoundedIntText)
+    # broken info.json for drive-1
+    (tmp_path / "system" / "drive-1" / "info.json").write_text('{"drive_number": 1')
+
+    # correct info.json for drive-2
+    info_text = textwrap.dedent(
+        """
+        {
+            "drive_number": 2,
+            "date": "2025-05-31",
+            "time": "20:29:33",
+            "start_time": "2025-05-31T20:29:33",
+            "adapter": "micromagneticdata",
+            "adapter_version": "0.65.0",
+            "driver": "MinDriver",
+            "end_time": "2025-05-31T20:29:33",
+            "elapsed_time": "00:00:01",
+            "success": true}
+        """
+    )
+    (tmp_path / "system" / "drive-2" / "info.json").write_text(info_text)
+
+    data = mdata.Data(name="system", dirname=str(tmp_path))
+    info = data.info
+    assert info["info.json"].to_list() == ["missing", "corrupt", "available"]
+
+
+def test_n(data):
+    assert data.n == 4
+
+
+def test_getitem(data):
+    for i in range(-4, 4):
+        assert isinstance(data[i], mdata.Drive)
+
+
+def test_iter(data):
+    assert len(list(data)) == 4
+
+
+def test_selector(data):
+    assert isinstance(data.selector(), ipywidgets.BoundedIntText)
